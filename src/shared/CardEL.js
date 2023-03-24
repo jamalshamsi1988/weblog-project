b9179f3596f69e935a3d2b22276c5f89d8653709
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-03-25 01:08:12 +0330

    add card element and added some styles

--- src/shared/CardEL.js
+++ src/shared/CardEL.js
@@ -1,10 +1,25 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography,Divider } from '@mui/material'
 
-const CardEL = (props) => {
+
+const CardEL = ({title,author,coverPhoto,slug}) => {
   return (
-    <div>
-      Card
-    </div>
+    <Card sx={{boxShadow:"rgba(0,0,0,0.1) 0 4px 12px" , borderRadius:4}}>
+      <CardHeader avatar={<Avatar src={author.avatar.url} sx={{marginLeft:2}} />} title={<Typography component="p" variant='p' color="text.secondary">{author.name}</Typography>} />
+      <CardMedia component="img" height="194" image={coverPhoto.url} alt={slug} />
+      <CardContent>
+            <Typography component="h3" variant='p' color="text.primary" fontWeight="600">{title}</Typography>
+      </CardContent>
+           <Divider variant="middle" sx={{margin:"10px"}}/>
+    {/* <Link to={`/blogs/${slug}`} style={{width:"100%" , textDecoration:"none"}}>      */}
+      <CardActions >
+        <Button variant="outlined" sx={{width:"100%" , borderRadius:3}} size="small" >
+         مطالعه مقاله
+        </Button>
+      </CardActions>
+      {/* </Link>  */}
+    </Card>
   )
 }
 

f1b298bb76f4c8826f1d4dc1053617d9fd8aa9c4
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-03-25 17:32:12 +0330

    import author data in AuthorPage.code was not working yet

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -1,25 +1,71 @@
-import React from 'react'
-import { useParams } from 'react-router'
-import { useQuery } from '@apollo/client';
-import { GET_AUTHOR_INFO } from '../graphql/quaries';
+// 
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router";
+// import sanitizeHtml from "sanitize-html";
+import { GET_AUTHOR_INFO } from "../graphql/quaries";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
+import CardEL from "../../shared/CardEL";
 
-const AuthorPage = () => {
 
-    const {slug} =useParams();
+function AuthorPage() {
+  const { slug } = useParams();
 
-    const {loading,data,errors}=useQuery(GET_AUTHOR_INFO, {
-        variables:{slug},
-    });
+  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO, {
+    variables: { slug },
+  });
+  
 
-    if(loading) return <h3>Loading ...</h3>
-    if(errors) return <h3>Error ...</h3>
-    console.log({slug});
+  if (errors) return <h3>Error...</h3>;
+
+  const {
+    author: { name, field, avatar, description, posts },
+  } = data;
 
   return (
-    <div>
-      author page
-    </div>
-  )
+    <Container maxWidth="lg">
+      <Grid container mt={10}>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Avatar src={avatar.url} sx={{ width: 250, height: 250 }} />
+          <Typography component="h3" variant="h5" fontWeight={700} mt={4}>
+            {name}
+          </Typography>
+          <Typography component="p" variant="h5" color="text.secondary" mt={2}>
+            {field}
+          </Typography>
+        </Grid>
+        {/* <Grid item xs={12} mt={5}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: sanitizeHtml(description.html),
+            }}
+          ></div>
+        </Grid> */}
+        <Grid item xs={12} mt={6}>
+          <Typography component="h3" variant="h5" fontWeight={700}>
+            مقالات {name}
+          </Typography>
+          <Grid container spacing={2} mt={2}>
+            {posts.map((post) => (
+              <Grid item xs={12} sm={6} md={4} key={post.id}>
+                <CardEL
+                  title={post.title}
+                  slug={post.slug}
+                  coverPhoto={post.coverPhoto}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default AuthorPage
+export default AuthorPage;
